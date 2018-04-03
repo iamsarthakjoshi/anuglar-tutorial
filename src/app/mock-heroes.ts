dcf793d72efa44bf5dc73bac0086e0c9450c2808
Sarthak Joshi <Nevermind@MacBook-Pro.local>
2018-04-03 07:34:49 +0545

    Indentation Improved

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,9 +1,9 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    {id:11, name: 'Mr. Nice'},
-    {id:12, name:'Narcos'},
-    {id:13, name:'Bombasto'},
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narcos' },
+    { id: 13, name: 'Bombasto' },
     { id: 14, name: 'Celeritas' },
     { id: 15, name: 'Magneta' },
     { id: 16, name: 'RubberMan' },
